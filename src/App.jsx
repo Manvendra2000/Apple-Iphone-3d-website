297cc127218ce6f8cfe6a6ef1686a640961cbde5
Manvendra2000 <mzealpro@gmail.com>
2024-09-11 20:21:57 +0530

    all sections finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,24 @@
-import Hero from "./components/Hero"
-import Highlights from "./components/Highlights"
-import Navbar from "./components/Navbar"
-import Model from "./components/Model"
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Highlights from './components/Highlights';
+import Model from './components/Model';
+import Features from './components/Features';
+import HowItWorks from './components/HowItWorks';
+import Footer from './components/Footer';
 
 import * as Sentry from '@sentry/react';
-import Features from "./components/Features";
-import HowItWorks from "./components/HowItWorks";
 
 const App = () => {
-  // return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
   return (
-    <>
-       <main className="bg-black">
-        <Navbar/>
-        <Hero/>
-        <Highlights/>
-        <Model />
-        <Features />
-        <HowItWorks/>
-       </main>
-    </>
+    <main className="bg-black">
+      <Navbar />
+      <Hero />
+      <Highlights />
+      <Model />
+      <Features />
+      <HowItWorks />
+      <Footer />
+    </main>
   )
 }
 
