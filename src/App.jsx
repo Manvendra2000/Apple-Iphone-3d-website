b27927f928b11e87bbec7aa688f2c9968976ceb9
Manvendra2000 <mzealpro@gmail.com>
2024-09-11 19:55:17 +0530

    how it works section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Model from "./components/Model"
 
 import * as Sentry from '@sentry/react';
 import Features from "./components/Features";
+import HowItWorks from "./components/HowItWorks";
 
 const App = () => {
   // return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
@@ -16,6 +17,7 @@ const App = () => {
         <Highlights/>
         <Model />
         <Features />
+        <HowItWorks/>
        </main>
     </>
   )
